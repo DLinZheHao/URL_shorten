c4cc0ec9d4aeae60e28039a9789078da2521db3f
DLinZheHao <BDLinOrsWJ0528@gmail.com>
2023-02-08 23:08:13 +0800

    AC要求更正：更新Model 兩個欄位 unique flag

--- models/urlModel.js
+++ models/urlModel.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 
-const urlSchema = new mongoose.Schema(
-    {
-        original: {
-            type: String,
-            required: [true, '輸入資料沒有目標網址！']
-        },
-        shorten: {
-            type: String,
-            required: [true, '輸入資料沒有短網址！']
-        }
-    }
-)
+const urlSchema = new mongoose.Schema({
+  original: {
+    type: String,
+    required: [true, '輸入資料沒有目標網址！'],
+    unique: true,
+  },
+  shorten: {
+    type: String,
+    required: [true, '輸入資料沒有短網址！'],
+    unique: true,
+  },
+});
 
 const Url = mongoose.model('Url', urlSchema);
 module.exports = Url;
